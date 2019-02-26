a281cdc08c640531f0381b702aa4f837e190b2d1
Pavel Titov <ptitov@gmail.com>
2019-02-26 21:59:16 +0300

    Load dynamically inserted scripts synchronyously

--- includes/includes.js
+++ includes/includes.js
@@ -1,20 +1,8 @@
-var footerLoadedEvent = new Event('footerLoaded')
-
 $(function() {
   $.ajax({
     url: "/includes/footer.html",
   })
     .done(function( data ) {
       $('body').append(data);
-      if (document.readyState == 'complete') {
-        // document already ready if all resources referenced in included file
-        // are available in cache
-        window.dispatchEvent(footerLoadedEvent)
-      } else {
-        // wait for resources to load
-        window.addEventListener("load", function(event) {
-          window.dispatchEvent(footerLoadedEvent)
-        })
-      }
     });
 });
