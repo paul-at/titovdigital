c3079266015d78cc6c65f7837a16942d978dd354
Pavel Titov <ptitov@gmail.com>
2019-02-26 21:48:41 +0300

    Use custom event for footer initialisation

--- includes/includes.js
+++ includes/includes.js
@@ -1,8 +1,11 @@
+var footerLoadedEvent = new Event('footerLoaded')
+
 $(function() {
   $.ajax({
     url: "/includes/footer.html",
   })
     .done(function( data ) {
       $('body').append(data);
+      window.dispatchEvent(footerLoadedEvent)
     });
 });
